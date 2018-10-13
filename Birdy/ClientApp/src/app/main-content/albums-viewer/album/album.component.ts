61af7660518e1e04895793a766f9d505c234387e
Johnson Pan <jpan@int.funkygeek.me>
2018-10-13 13:20:20 -0700

    Implement album preview

--- Birdy/ClientApp/src/app/main-content/albums-viewer/album/album.component.ts
+++ Birdy/ClientApp/src/app/main-content/albums-viewer/album/album.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { Album } from '../../../models/album';
+import { PhotoService } from 'src/app/photo.service';
+import { Photo } from 'src/app/models/photo';
 
 @Component({
   selector: 'app-album',
@@ -8,11 +10,16 @@ import { Album } from '../../../models/album';
 })
 export class AlbumComponent implements OnInit {
 
-  @Input() album : Album;
+  @Input() album: Album;
   @Output() clicked = new EventEmitter<Album>();
-  constructor() { }
+
+  previewPhotos: Photo[];
+  constructor(private photoService: PhotoService) {  }
 
   ngOnInit() {
+    this.photoService.getAlbum(this.album.albumSetId, this.album.id).subscribe(album => {
+      this.previewPhotos = album.getPhotos().slice(0, 4);
+    });
   }
 
   onClickMe() {
