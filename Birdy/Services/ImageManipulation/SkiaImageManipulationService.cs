eb6328ec3f72771ab4a952934b629f3073e579ef
Johnson Pan <jpan@int.funkygeek.me>
2018-10-26 01:25:32 -0700

    Add SkiaImageManipulationService

---
+++ Birdy/Services/ImageManipulation/SkiaImageManipulationService.cs
@@ -0,0 +1,74 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace Birdy.Services.ImageManipulation
+{
+    public class SkiaImageManipulationService : IImageManipulationService
+    {
+        public Task<byte[]> GenerateHdImageAsync(byte[] imageData)
+        {
+            return Task.Run(() =>
+            {
+                return resize(imageData, 1280);
+            });
+        }
+
+        public Task<byte[]> GenerateThumbnailImageAsync(byte[] imageData)
+        {
+            return Task.Run(() =>
+            {
+                return resize(imageData, 160);
+            });
+        }
+
+        private byte[] resize(byte[] imageData, int size)
+        {
+            using (var inputStream = new SKManagedStream(new MemoryStream(imageData)))
+            {
+                using (var original = SKBitmap.Decode(inputStream))
+                {
+                    int width, height;
+                    if (original.Width > original.Height)
+                    {
+                        width = size;
+                        height = original.Height * size / original.Width;
+                    }
+                    else
+                    {
+                        width = original.Width * size / original.Height;
+                        height = size;
+                    }
+
+                    using (var resized = original.Resize(new SKImageInfo(width, height), SKBitmapResizeMethod.Lanczos3))
+                    {
+                        if (resized == null) return null;
+                        using (var image = SKImage.FromBitmap(resized))
+                        {
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                image.Encode(SKEncodedImageFormat.Jpeg, 90).SaveTo(ms);
+                                return ms.ToArray();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private Tuple<int, int> calculateResolution(Tuple<int, int> input, int target)
+        {
+            if (input.Item1 > input.Item2)
+            {
+                double scaleFactor = input.Item2 / (double)target;
+                return new Tuple<int, int>(Convert.ToInt32(input.Item1 / scaleFactor), target);
+            }
+            else
+            {
+                double scaleFactor = input.Item1 / (double)target;
+                return new Tuple<int, int>(target, Convert.ToInt32(input.Item2 / scaleFactor));
+            }
+        }
+    }
+}
